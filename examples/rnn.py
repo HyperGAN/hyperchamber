b8017dd5b898fdfb887241381df24caf0e77a2f9
Martyn Garcia <martyn@255bits.com>
2016-05-13 02:10:07 -0600

    [wip] rnn example with baseline lstm

--- examples/rnn.py
+++ examples/rnn.py
@@ -1,27 +1,83 @@
+#inspired by http://people.idsia.ch/~juergen/icann2009bayer.pdf
+
 import tensorflow as tf
 import hyperchamber as hc
 
+hc.set("learning_rate", 0.01)
 hc.set("batch_size", 128)
+hc.set('rnn_size', 4)
 
-def create_graph(x):
+TRAIN_STEPS=100
+TESTS = TRAIN_STEPS/10
 
-def train():
+def rnn(x, scope='rnn'):
+    with tf.variable_scope(scope):
+        memory=hc.get('rnn_size')
+        cell = rnn_cell.BasicLSTMCell(memory)
+        x, state = rnn.rnn(cell, [x], dtype=tf.boolean)
+        #w = tf.get_variable('w', [hc.get('rnn_size'),4])
+        #b = tf.get_variable('b', [4])
+        #x = tf.nn.xw_plus_b(x, w, b)
+        x=tf.sign(x)
+        return x, state
+
+# Each step of the graph we have:
+# x is [BATCH_SIZE, 4] where the data is an one hot binary vector of the form:
+# [start_token end_token a b]
+#
+# y is [BATCH_SIZE, 4] is a binary vector of the chance each character is correct
+#
+def create_graph(x, y):
+    output, state = rnn(x)
+    hc.set("cost", tf.sum(tf.reduce_mean(output*tf.log(y))))
 
 def run():
     rand = np.random_uniform()#TODO as int
-    graph = create_graph(x)
-    batch = get_an_bn_grammar(rand)
-    _, cost = sess.run(train_step, graph['cost'])
+    create_graph(x, y)
+    x_input = []
+    y_input = []
+    console.log("Training each RNN")
+    for i in range(TRAIN_STEPS):
+        _, costs = hc.parallel.run(sess, train_step, hc.get('cost'), graphs=5)
+        hc.cost(costs)
 
-# returns a vector of a**nb**n grammars given a set of integer values x, denoting a list of 'n's
-# for example
-# get_an_bn_grammar(0) # empty string
-# get_an_bn_grammar(1) # an
-# get_an_bn_grammar(3) # aaannn
+    for i in range(TESTS):
+        _, costs = hc.parallel.run(sess, hc.get('cost'), graphs=5)
+        print("costs are", costs)
+        hc.test(costs)
+
+    for rnn in hc.in_top_k(3):
+        print(rnn)
+
+def encode(chars):
+    def vectorize(c):
+        if(c=="S"):
+            return [1,0,0,0]
+        if(c=="T"):
+            return [0,1,0,0]
+        if(c=="a"):
+            return [0,0,1,0]
+        if(c=="b"):
+            return [0,0,0,1]
+    return [vectorize(c) for c in chars]
+
+
+def get_an_bn(n):
+    return encode("a"*n+"b"*n)
+def get_an_bn_an(n):
+    return encode("a"*n+"b"*n+"a"*n)
+
+# returns a vector of a**nb**n grammars given a set of one-hot vectors x, denoting the next element in a generated sequence
+# Each value is returned as an array of possible next inputs.
+# for example:
+# get_an_bn_grammar(ST) # T
+# get_an_bn_grammar(SabT) # T/a-b-T
+# get_an_bn_grammar(SaaabbbT) # T/a-a/b-a/b-b-b-b-T
+#
+# where S and T designate start and end symbols
 def get_an_bn_grammar(x):
     return ""
 
 # same as above but with extra 'a's
 def get_an_bn_an_grammar(x):
     return ""
-
