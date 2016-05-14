79e409a1c719b5bf506ed6250aab2225bf1ab89b
Martyn Garcia <martyn@255bits.com>
2016-05-14 14:51:26 -0600

    [wip] initial set/config

--- hyperchamber/__init__.py
+++ hyperchamber/__init__.py
@@ -1,8 +1,20 @@
+store = {}
 def set(key, value):
-    return None
+    store[key]=value
+    return store
 
 def configs(max=1):
-    return []
+    if(len(store)==0):
+        return []
+    configs = []
+    for i in range(max):
+        config = {}
+        for k in store:
+            rand=store[k][0]
+            config[k]=rand
+        configs.append(config)
+    return configs
 
 def reset():
+    store = {}
     return
--- tests/test_hyperchamber.py
+++ tests/test_hyperchamber.py
@@ -6,10 +6,18 @@ import hyperchamber as hc
 
 
 class hyperchamber_test(unittest.TestCase):
+    def test_set(self):
+        hc.reset()
+        hc.set('x', [1])
+        self.assertEqual(hc.configs(1), [{'x':1}])
 
-  def test_gets_empty_config(self):
-      hc.reset()
-      self.assertEqual(hc.configs(), [])
+    def test_configs(self):
+        hc.reset()
+        self.assertEqual(hc.configs(), [])
+
+    def test_reset(self):
+        hc.reset()
+        self.assertEqual(hc.configs(), [])
 
 if __name__ == '__main__':
     unittest.main()
