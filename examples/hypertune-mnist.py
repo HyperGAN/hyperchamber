01adb5475b4532c342da1dac4c04b4d98b21a8fe
Mikkel Garcia <mikkel@255bits.com>
2016-05-14 16:51:02 -0600

    [feature] example 1 runs/fails, doesnt use hyperchamber yet

--- examples/hypertune-mnist.py
+++ examples/hypertune-mnist.py
@@ -1,4 +1,6 @@
-import ../../hyperchamber as hc
+import hyperchamber as hc
+import shared.mnist_data as mnist
+from shared.ops import *
 
 import os
 import time
@@ -7,33 +9,37 @@ import tensorflow as tf
 
 learning_rates = [ 2e-3, 2e-4 ]
 hc.set("learning_rate", learning_rates)
+hc.set("adam_beta1", [0.9]*len(learning_rates))
 
 hc.set("batch_size", [128] * len(learning_rates))
 hc.set("x_dims", [[26, 26] * len(learning_rates)])
-hc.set("y_dims", [[19] * len(learning_rates)])
+hc.set("y_dims", [[10] * len(learning_rates)])
 
 #def validate(value):
 #    return value != value #NaN
 
 #hc.evolve.evolve("learn_rate", 0.2, validate)
 
-def hidden_layers(x):
-    return x*w + b
+def hidden_layers(config, x):
+    return x
+
+def output_layer(config, x):
+    return linear(x, config["y_dims"])
 
 def create(config):
-    batch_size = config.batch_size
-    x = tf.placeholder(tf.float32, [batch_size, *config.x_size], name="x")
-    y = tf.placeholder(tf.float32, [batch_size, *config.y_size], name="y")
+    batch_size = config["batch_size"]
+    x = tf.placeholder(tf.float32, [batch_size, *config["x_dims"]], name="x")
+    y = tf.placeholder(tf.float32, [batch_size, config["y_dims"]], name="y")
 
     hidden = hidden_layers(config, x)
     output = output_layer(config, hidden)
 
+    loss = tf.nn.softmax_cross_entropy_with_logits(output, y) 
 
-    opt = tf.train.AdamOptimizer(self.learning_rate_d, beta1=self.beta1, name="optimizer") \
+    opt = tf.train.AdamOptimizer(loss, beta1=config["adam_beta1"], name="optimizer") \
                                       .minimize(self.d_loss, var_list=self.d_vars)
-    # cost = Xent
 
-def train(sess, x_input, y_labels):
+def train(sess, config, x_input, y_labels):
     x = hc.tf.getTensor("x")
     y = hc.tf.getTensor("y")
     cost = hc.tf.getTensor("cost")
@@ -41,16 +47,29 @@ def train(sess, x_input, y_labels):
 
     cost = sess.run([cost], feed_dict={x:x_input, y:y_labels})
 
-    hc.cost(cost)
+    #hc.cost(config, cost)
     print("Cost "+str(cost))
 
 def epoch(sess):
     # TODO: load mnist and train on data
+    print("TODO")
 
-for config in hc.configs(1):
-    config = config[0]
+configs = [
+    {
+        "learning_rate": 2e-3,
+        "adam_beta1": 0.9, 
+        "batch_size": 128,
+        "x_dims": [26, 26],
+        "y_dims": 10
+    }
+]
+
+for config in configs:
+#for config in hc.configs(1):
+#    config = config[0]
     print("Testing configuration", config)
     sess = tf.Session()
+    graph = create(config)
     epoch(sess)
     sess.close()
     print("Done testing.  Final cost was:", hc.cost())
---
+++ examples/shared/ops.py
@@ -0,0 +1,19 @@
+
+import math
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.python.framework import ops
+
+def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
+    shape = input_.get_shape().as_list()
+
+    with tf.variable_scope(scope or "Linear"):
+        matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+                                 tf.random_normal_initializer(stddev=stddev))
+        bias = tf.get_variable("bias", [output_size],
+            initializer=tf.constant_initializer(bias_start))
+        if with_w:
+            return tf.matmul(input_, matrix) + bias, matrix, bias
+        else:
+            return tf.matmul(input_, matrix) + bia
