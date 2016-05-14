c432b543f3c581bb1526e7b0fc98f1eec1a1e175
Martyn Garcia <martyn@255bits.com>
2016-05-14 15:15:24 -0600

    [test] 2 vars 2 config

--- tests/test_hyperchamber.py
+++ tests/test_hyperchamber.py
@@ -16,6 +16,13 @@ class hyperchamber_test(unittest.TestCase):
         hc.set('x', [1,2])
         self.assertEqual(hc.configs(2), [{'x':1},{'x':2}])
 
+    def test_set2_2vars(self):
+        hc.reset()
+        hc.set('x', [1,2])
+        hc.set('y', [3,4])
+        self.assertEqual(hc.configs(2), [{'x':1,'y':3},{'x':2,'y':4}])
+
+
 
     def test_configs(self):
         hc.reset()
