6939bfb5b11b6456c7f2228cbec874d9029bfd34
Martyn Garcia <martyn@255bits.com>
2016-05-16 14:05:27 -0600

    [feature] hc integration on mnist

--- examples/hypertune-mnist.py
+++ examples/hypertune-mnist.py
@@ -7,13 +7,13 @@ import time
 import numpy as np
 import tensorflow as tf
 
-learning_rates = [ 2e-3, 2e-4 ]
+learning_rates = [ 2e-3 ]
 hc.set("learning_rate", learning_rates)
-hc.set("adam_beta1", [0.9]*len(learning_rates))
+hc.set("adam_beta1", [0.9])
 
-hc.set("batch_size", [128] * len(learning_rates))
-hc.set("x_dims", [[26, 26] * len(learning_rates)])
-hc.set("y_dims", [[10] * len(learning_rates)])
+hc.set("batch_size", [128])
+hc.set("x_dims", [[26, 26]])
+hc.set("y_dims", [10])
 
 #def validate(value):
 #    return value != value #NaN
@@ -22,8 +22,8 @@ hc.set("y_dims", [[10] * len(learning_rates)])
 
 def hidden_layers(config, x):
     output = tf.reshape(x, [config["batch_size"], config["x_dims"][0]*config["x_dims"][1]])
-    #output = linear(output, 1024, scope="l2")
-    #output = tf.nn.tanh(output)
+    output = linear(output, 26*26, scope="l2")
+    output = tf.nn.tanh(output)
     return output
 
 def output_layer(config, x):
@@ -31,18 +31,18 @@ def output_layer(config, x):
 
 def create(config):
     batch_size = config["batch_size"]
-    x = tf.placeholder(tf.float32, [batch_size, *config["x_dims"], 1], name="x")
+    x = tf.placeholder(tf.float32, [batch_size, config["x_dims"][0], config["x_dims"][1], 1], name="x")
     y = tf.placeholder(tf.float32, [batch_size, config["y_dims"]], name="y")
 
     hidden = hidden_layers(config, x)
     output = output_layer(config, hidden)
 
-    output = tf.nn.softmax(output)
-    loss  = tf.reduce_mean(-tf.reduce_sum(y * tf.log(output), reduction_indices=[1]))
-
-    #loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(output, y), name="loss")
-
     #output = tf.nn.softmax(output)
+    #loss  = tf.reduce_mean(-tf.reduce_sum(y * tf.log(output), reduction_indices=[1]))
+
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(output, y), name="loss")
+
+    output = tf.nn.softmax(output)
     correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(y,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -81,26 +81,14 @@ def epoch(sess, config):
         x, y = mnist.next_batch(batch_size, with_label=True)
         train(sess, config, x, y)
 
-configs = [
-    {
-        "learning_rate": 2e-3,
-        "adam_beta1": 0.9, 
-        "batch_size": 128,
-        "x_dims": [26, 26],
-        "y_dims": 10
-    }
-]
-
-for config in configs:
-#for config in hc.configs(1):
-#    config = config[0]
+for config in hc.configs(1):
+    print(config)
     print("Testing configuration", config)
     sess = tf.Session()
     graph = create(config)
     init = tf.initialize_all_variables()
     sess.run(init)
-    for i in range(1000):
-        epoch(sess, config)
+    epoch(sess, config)
     sess.close()
     #print("Done testing.  Final cost was:", hc.cost())
 
