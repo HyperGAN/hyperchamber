ada563bd7ec13b94d88956e19bb1c3721c4f602d
Martyn Garcia <martyn@255bits.com>
2016-05-18 19:14:29 -0600

    [fix] hypergan mvp working

--- examples/hypertune-gan.py
+++ examples/hypertune-gan.py
@@ -7,12 +7,19 @@ import time
 import numpy as np
 import tensorflow as tf
 
+import matplotlib
+import matplotlib.pyplot as plt
+
 from tensorflow.python.framework import ops
 
-hc.set("g_learning_rate", 0.7)
-hc.set("d_learning_rate", 0.7)
-hc.set("g_layers", [10])
-hc.set("d_layers", [20])
+hc.set("g_learning_rate", 0.2)
+hc.set("d_learning_rate", 0.2)
+
+g_layers = [ [],[26*26, 26*26], [26*26],  [128], [64, 64], [16, 32], [26,26] ]
+d_layers = [ [],[26*26, 26*26], [26*26],  [128], [64, 64], [16, 32], [26,26] ]
+
+hc.set("g_layers", g_layers)
+hc.set("d_layers", d_layers)
 hc.set("z_dim", 64)
 
 hc.set("batch_size", 128)
@@ -20,11 +27,15 @@ hc.set("batch_size", 128)
 X_DIMS=[26,26]
 
 def generator(config):
+    output_shape = X_DIMS[0]*X_DIMS[1]
     z = tf.random_uniform([config["batch_size"], 64],0,1)
-    result = linear(z, X_DIMS[0]*X_DIMS[1], scope="g_proj0")
-    result = tf.nn.tanh(result)
+    result = z
 
-    result = linear(result, X_DIMS[0]*X_DIMS[1], scope="g_proj")
+    for i, layer in enumerate(config['g_layers']):
+        result = linear(result, layer, scope="g_linear_"+str(i))
+        result = tf.nn.tanh(result)
+    if(result.get_shape()[1] != output_shape):
+        result = linear(result, output_shape, scope="g_proj")
     result = tf.reshape(result, [config["batch_size"], X_DIMS[0], X_DIMS[1]])
     return result
 
@@ -86,17 +97,34 @@ def test(sess, config, x_input, y_labels):
     x = get_tensor("x")
     d_fake = get_tensor("d_fake")
     d_real = get_tensor("d_real")
+    g_loss = get_tensor("g_loss")
 
-    d_fake_cost, d_real_cost = sess.run([d_fake, d_real], feed_dict={x:x_input})
-
-
-    print("test g_loss %.2f d_loss %.2f" % (d_fake_cost, d_real_cost))
-    return d_fake_cost, d_real_cost
+    g_cost, d_fake_cost, d_real_cost = sess.run([g_loss, d_fake, d_real], feed_dict={x:x_input})
 
 
-def epoch(sess, config):
+    #hc.event(costs, sample_image = sample[0])
+    
+    print("test g_loss %.2f d_fake %.2f d_loss %.2f" % (g_cost, d_fake_cost, d_real_cost))
+    return g_cost,d_fake_cost, d_real_cost
+
+
+def sample(sess, config):
+    generator = get_tensor("g")
+    sample = sess.run([generator])
+    return sample[0][0]
+
+def plot_mnist_digit(image, file):
+    """ Plot a single MNIST image."""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    print(np.array(image).shape)
+    ax.matshow(image, cmap = matplotlib.cm.binary)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    plt.savefig(file)
+
+def epoch(sess, config, mnist):
     batch_size = config["batch_size"]
-    mnist = read_data_sets(one_hot=True)
     n_samples = mnist.num_examples
     total_batch = int(n_samples / batch_size)
     for i in range(total_batch):
@@ -108,42 +136,58 @@ def test_config(sess, config):
     mnist = read_test_sets(one_hot=True)
     n_samples = mnist.num_examples
     total_batch = int(n_samples / batch_size)
-    accuracies = []
-    costs = []
+    results = []
     for i in range(total_batch):
         x, y = mnist.next_batch(batch_size, with_label=True )
-        accuracy, cost = test(sess, config, x, y)
-        accuracies.append(accuracy)
-        costs.append(cost)
-    return accuracies, costs
+        results.append(test(sess, config, x, y))
+    return results
 
 
+mnist = read_data_sets(one_hot=True)
+j=0
 for config in hc.configs(100):
     print("Testing configuration", config)
     sess = tf.Session()
     graph = create(config)
     init = tf.initialize_all_variables()
     sess.run(init)
-    epoch(sess, config)
-    accuracies, costs = test_config(sess, config)
-
-    # calculate D.difficulty = reduce_mean(d_loss) - reduce_mean(d_loss_fake)
+    for i in range(10):
+        epoch(sess, config, mnist)
+    results = test_config(sess, config)
+    loss = np.array(results)
+    #results = np.reshape(results, [results.shape[1], results.shape[0]])
+    g_loss = [g for g,_,_ in loss]
+    g_loss = np.mean(g_loss)
+    d_fake = [d_ for _,d_,_ in loss]
+    d_fake = np.mean(d_fake)
+    d_real = [d for _,_,d in loss]
+    d_real = np.mean(d_real)
+    # calculate D.difficulty = reduce_mean(d_loss_fake) - reduce_mean(d_loss_real)
+    difficulty = d_real * (1-d_fake)
     # calculate G.ranking = reduce_mean(g_loss) * D.difficulty
-    accuracy, cost = np.mean(accuracies), np.mean(costs)
+    ranking = g_loss * (1.0-difficulty)
+
+    s = sample(sess, config)
+    plot_mnist_digit(s, "samples/config-"+str(j)+".png")
+    j+=1
     results =  {
-        'accuracy':accuracy,
-        'cost':cost
+        'difficulty':difficulty,
+        'ranking':ranking,
+        'g_loss':g_loss,
+        'd_fake':d_fake,
+        'd_real':d_real,
+        'sample':s
         }
     hc.record(config, results)
     ops.reset_default_graph()
     sess.close()
 
 
-def by_accuracy(x):
+def by_ranking(x):
     config,result = x
-    return 1-result['accuracy']
+    return result['ranking']
 
-for config, result in hc.top(by_accuracy):
+for config, result in hc.top(by_ranking):
     print("RESULTS")
     print(config, result)
     
