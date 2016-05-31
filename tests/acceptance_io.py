c1dfa7db81281367d0de6aa4e741500d55bd15dc
Mikkel Garcia <mikkel@255bits.com>
2016-05-31 14:53:12 -0600

    [refactor] acceptance io

--- tests/acceptance_io.py
+++ tests/acceptance_io.py
@@ -4,26 +4,31 @@ import numpy as np
 
 import hyperchamber as hc
 import matplotlib.pyplot as plt
-plt.rcdefaults()
 
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(people))
-performance = 3 + 10 * np.random.rand(len(people))
-error = np.random.rand(len(people))
+def test_graph(config, filename):
+  plt.rcdefaults()
 
-plt.barh(y_pos, performance, xerr=error, align='center', alpha=0.4)
-plt.yticks(y_pos, people)
-plt.xlabel('Performance')
-plt.title('Are intrinsic measurements working?')
+  people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+  y_pos = np.arange(len(people))
+  performance = 3 + 10 * np.random.rand(len(people))
+  error = np.random.rand(len(people))
 
-plt.savefig("/tmp/acceptance_io.png")
+  plt.barh(y_pos, performance, xerr=error, align='center', alpha=0.4)
+  plt.yticks(y_pos, people)
+  plt.xlabel('Performance')
+  plt.title('Are intrinsic measurements working?')
+
+  plt.savefig(filename)
 
 #hc.io.apikey("TODO")
 
 hc.set("model", "255bits/acceptance_test")
 hc.set("version", "0.0.1")
+hc.set("test", "acceptance_io")
 
-config = {'test': 'acceptance_io'}
-hc.io.sample(config, ["/tmp/acceptance_io.png"])
+for config in hc.configs(1):
+  filename = "/tmp/acceptance_io.png"
+  hc.io.sample(config, [filename])
 
-hc.io.record(config, {'done': True})
+  hc.io.record(config, {'done': True})
+  print("Stored config", config)
