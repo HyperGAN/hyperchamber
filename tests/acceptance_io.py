1f1af86c871e86bdc461923fd1d88f348e49c907
Mikkel Garcia <mikkel@255bits.com>
2016-05-31 12:50:03 -0600

    [fix] missing acceptance test

---
+++ tests/acceptance_io.py
@@ -0,0 +1,27 @@
+import unittest
+import tensorflow as tf
+import numpy as np
+
+import hyperchamber as hc
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+plt.barh(y_pos, performance, xerr=error, align='center', alpha=0.4)
+plt.yticks(y_pos, people)
+plt.xlabel('Performance')
+plt.title('Are intrinsic measurements working?')
+
+plt.savefig("/tmp/acceptance_io.png")
+
+#hc.io.apikey("TODO")
+hc.io.model("sanity")
+
+config = {'test': 'acceptance_io'}
+hc.io.sample(config, ["/tmp/acceptance_io.png"])
+
+hc.io.record(config, {'done': True})
