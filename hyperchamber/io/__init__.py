a90d2ea43e9db4d7bc348d522c00b10722709133
Mikkel Garcia <mikkel@255bits.com>
2016-05-30 18:31:22 -0600

    [fix] send json

--- hyperchamber/io/__init__.py
+++ hyperchamber/io/__init__.py
@@ -13,21 +13,16 @@ def model(model):
 
 def sample(config, images):
   """Upload a series of images.  Images are ignored if the rate limit is hit."""
-  print("sample")
   url = get_api_path('intrinsic.json')
-  print(url)
   multiple_files = []
   for image in images:
     multiple_files.append(('images', (image, open(image, 'rb'), 'image/png')))
-  r = requests.post(url, data=config, files=multiple_files, timeout=5)
-  print("response", r)
+  r = requests.post(url, json=config, files=multiple_files, timeout=5)
   return r.text
 
 def record(config, result):
   """Records results on hyperchamber.io.  Used when you are done testing a config."""
-  print("record")
   url = get_api_path('run.json')
   data = {'config': config, 'result': result}
-  r = requests.post(url, data=data, timeout=30)
-  print("response", r)
+  r = requests.post(url, json=data, timeout=30)
   return r.text
