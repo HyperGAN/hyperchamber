5d3b85f2b2ab478a3a692b8733638217c04ebb68
Martyn Garcia <martyn@255bits.com>
2016-06-03 22:52:23 -0600

    [feature] batchnorm and regularization

--- examples/cifar.py
+++ examples/cifar.py
@@ -15,25 +15,32 @@ from tensorflow.python.framework import ops
 from tensorflow.models.image.cifar10 import cifar10_input
 import shared.cifar_utils as cifar_utils
 
-start=.00001
+start=.0001
 end=.01
 num=20
 hc.permute.set("g_learning_rate", list(np.linspace(start, end, num=num)))
 hc.permute.set("d_learning_rate", list(np.linspace(start, end, num=num)))
 
-conv_g_layers = [[64*4, 64*2, 1]]
+conv_g_layers = [[32*4, 32*2, 1], [64*4,64*2,1], [16*4,16*2,1]]
 
-conv_d_layers = [[64, 64*2, 64*4]]
+conv_d_layers = [[32, 32*2, 32*4],[64, 64*2, 64*4],[64,64*2]]
 
 hc.permute.set("conv_g_layers", conv_g_layers)
 hc.permute.set("conv_d_layers", conv_d_layers)
 
-hc.permute.set("z_dim", [64*8])
+hc.permute.set("z_dim", [32*8, 64*8, 16*8, 64])
+
+hc.permute.set("regularize", [False, True])
+hc.permute.set("regularize_lambda", list(np.linspace(0.0001, 1, num=30)))
+
+hc.permute.set("g_batch_norm", [True])
+hc.permute.set("d_batch_norm", [True])
 
 BATCH_SIZE=64
 hc.set("batch_size", BATCH_SIZE)
 hc.set("model", "255bits/cifar-gan")
 hc.set("version", "0.0.1")
+
 
 X_DIMS=[32,32]
 Y_DIMS=10
@@ -44,25 +51,28 @@ def generator(config, y):
     z = tf.random_uniform([config["batch_size"], 10],0,1)
     result = tf.concat(1, [y, z])
     result = linear(result, config['z_dim'], 'g_input_proj')
-    #result = tf.nn.dropout(result, 0.7)
 
     if config['conv_g_layers']:
         result = tf.reshape(result, [config['batch_size'], 4,4,config['z_dim']//16])
+        #result = tf.nn.dropout(result, 0.7)
+        if(config['g_batch_norm']):
+            result = batch_norm(result, name='g_lin_bn')
+        result = tf.nn.relu(result)
         for i, layer in enumerate(config['conv_g_layers']):
             j=int(result.get_shape()[1]*2)
             k=int(result.get_shape()[2]*2)
             output = [config['batch_size'], j,k,layer]
             result = deconv2d(result, output, scope="g_conv_"+str(i))
-            result = tf.nn.sigmoid(result)
+            if(config['g_batch_norm']):
+                result = batch_norm(result, name='g_conv_bn_'+str(i))
+            result = tf.nn.tanh(result)
         result = tf.reshape(result,[config['batch_size'], -1])
-    else:
-        for i, layer in enumerate(config['g_layers']):
-            result = linear(result, layer, scope="g_linear_"+str(i))
-            result = tf.nn.sigmoid(result)
 
-    if(result.get_shape()[1] != output_shape):
-        result = linear(result, output_shape, scope="g_proj")
+    result = linear(result, output_shape, scope="g_proj")
     result = tf.reshape(result, [config["batch_size"], X_DIMS[0], X_DIMS[1], 3])
+    if(config['g_batch_norm']):
+        result = batch_norm(result, name='g_lin_bn_out')
+    result = tf.nn.sigmoid(result)
     return result
 
 def discriminator(config, x, reuse=False):
@@ -72,13 +82,10 @@ def discriminator(config, x, reuse=False):
         result = tf.reshape(x, [config["batch_size"], X_DIMS[0],X_DIMS[1],3])
         for i, layer in enumerate(config['conv_d_layers']):
             result = conv2d(result, layer, scope='d_conv'+str(i))
+            if(config['d_batch_norm']):
+                result = batch_norm(result, name='d_conv_bn_'+str(i))
             result = tf.nn.relu(result)
         result = tf.reshape(x, [config["batch_size"], -1])
-    else:
-        result = tf.reshape(x, [config["batch_size"], -1])
-        for i, layer in enumerate(config['d_layers']):
-            result = linear(result, layer, scope="d_linear_"+str(i))
-            result = tf.nn.relu(result)
 
     #result = tf.nn.dropout(result, 0.7)
     last_layer = result
@@ -106,6 +113,15 @@ def create(config, x,y):
 
     g_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(d_fake, real_symbols))
     d_loss = tf.reduce_mean(0.5*d_fake_loss + 0.5*d_real_loss)
+
+    if config['regularize']:
+        ws = None
+        with tf.variable_scope("g_input_proj"):
+            tf.get_variable_scope().reuse_variables()
+            ws = tf.get_variable('Matrix')
+        lam = config['regularize_lambda']
+        g_loss += lam*tf.nn.l2_loss(ws)
+
 
     g_vars = [var for var in tf.trainable_variables() if 'g_' in var.name]
     d_vars = [var for var in tf.trainable_variables() if 'd_' in var.name]
@@ -201,6 +217,49 @@ def test_config(sess, config):
         results.append(test(sess, config))
     return results
 
+def test_epoch(epoch, j, sess, config):
+    #x = sample_input(sess, config)
+    #sample_file = "samples/input-"+str(j)+".png"
+    #cifar_utils.plot(config, x, sample_file)
+    
+    sample = samples(sess, config)
+    sample_list = []
+    for s in sample:
+        sample_file = "samples/config-"+str(j)+".png"
+        cifar_utils.plot(config, s, sample_file)
+        sample_list.append(sample_file)
+        j+=1
+    hc.io.sample(config, sample_list)
+    return j
+
+    ranking = 10000
+    results =  {
+    #    'difficulty':float(difficulty),
+        'ranking':float(ranking),
+    #    'g_loss':float(g_loss),
+    #    'd_fake':float(d_fake),
+    #    'd_real':float(d_real),
+        }
+
+
+def record_run(config, results):
+    #  results = test_config(sess, config)
+    #  loss = np.array(results)
+    #  #results = np.reshape(results, [results.shape[1], results.shape[0]])
+    #  g_loss = [g for g,_,_ in loss]
+    #  g_loss = np.mean(g_loss)
+    #  d_fake = [d_ for _,d_,_ in loss]
+    #  d_fake = np.mean(d_fake)
+    #  d_real = [d for _,_,d in loss]
+    #  d_real = np.mean(d_real)
+    #  # calculate D.difficulty = reduce_mean(d_loss_fake) - reduce_mean(d_loss_real)
+    #  difficulty = d_real * (1-d_fake)
+    #  # calculate G.ranking = reduce_mean(g_loss) * D.difficulty
+    #  ranking = g_loss * (1.0-difficulty)
+    hc.io.record(config, results)
+
+
+
 print("Generating configs with hyper search space of ", hc.count_configs())
 
 j=0
@@ -219,43 +278,11 @@ for config in hc.configs(100):
 
     for i in range(10):
         epoch(sess, config)
-    #  results = test_config(sess, config)
-    #  loss = np.array(results)
-    #  #results = np.reshape(results, [results.shape[1], results.shape[0]])
-    #  g_loss = [g for g,_,_ in loss]
-    #  g_loss = np.mean(g_loss)
-    #  d_fake = [d_ for _,d_,_ in loss]
-    #  d_fake = np.mean(d_fake)
-    #  d_real = [d for _,_,d in loss]
-    #  d_real = np.mean(d_real)
-    #  # calculate D.difficulty = reduce_mean(d_loss_fake) - reduce_mean(d_loss_real)
-    #  difficulty = d_real * (1-d_fake)
-    #  # calculate G.ranking = reduce_mean(g_loss) * D.difficulty
-    #  ranking = g_loss * (1.0-difficulty)
-
-    #x = sample_input(sess, config)
-    #sample_file = "samples/input-"+str(j)+".png"
-    #cifar_utils.plot(config, x, sample_file)
-    
-    sample = samples(sess, config)
-    sample_list = []
-    for s in sample:
-        sample_file = "samples/config-"+str(j)+".png"
-        cifar_utils.plot(config, s, sample_file)
-        sample_list.append(sample_file)
-        j+=1
-    hc.io.sample(config, sample_list)
-
-    ranking = 10000
-    results =  {
-    #    'difficulty':float(difficulty),
-        'ranking':float(ranking),
-    #    'g_loss':float(g_loss),
-    #    'd_fake':float(d_fake),
-    #    'd_real':float(d_real),
-        }
+        j=test_epoch(i, j, sess, config)
     #print("results: difficulty %.2f, ranking %.2f, g_loss %.2f, d_fake %.2f, d_real %.2f" % (difficulty, ranking, g_loss, d_fake, d_real))
-    hc.io.record(config, results)
+
+    record_run(config, results)
+    #with g.as_default():
     tf.reset_default_graph()
     sess.close()
 
