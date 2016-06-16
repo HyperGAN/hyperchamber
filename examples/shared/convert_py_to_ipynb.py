2df7eec81de1b219d90dde76c1c3a9891627b1cb
Martyn Garcia <martyn@255bits.com>
2016-06-16 13:30:20 -0600

    [wip] getting started notebook

---
+++ examples/shared/convert_py_to_ipynb.py
@@ -0,0 +1,11 @@
+from nbformat import v3, v4
+
+with open("mnist.py") as fpin:
+    text = fpin.read()
+
+    nbook = v3.reads_py(text)
+    nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
+
+    jsonform = v4.writes(nbook) + "\n"
+    with open("mnist.ipynb", "w") as fpout:
+        fpout.write(jsonform)
