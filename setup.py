64d91e9f61cd8805ce6166ad4fbac1f3dc9067eb
Martyn Garcia <martyn@255bits.com>
2016-12-25 16:11:35 -0700

    [fix] add missing package

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='hyperchamber',
-    version='0.2',
+    version='0.2.1',
     description='Tune and optimize your hyperparameters',
     author='Martyn Garcia',
     author_email="martyn@255bits.com",
-    packages=['hyperchamber'],
+    packages=['hyperchamber', 'hyperchamber.io'],
     install_requires=['requests >= 2.4.2'],
     url='https://github.com/255bits/hyperchamber',
     license='MIT',
